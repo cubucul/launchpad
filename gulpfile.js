6fe0c4615d2a7e65e41683efa9cc47bb642abb3a
Ilya Agafonov <cubucul@gmail.com>
2021-11-04 12:22:49 +0500

    Минифицирует html

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
 const { src, dest } = require('gulp');
 const postcss = require('gulp-postcss');
+const htmlmin = require('gulp-htmlmin');
+
+function html() {
+  return src('src/*.html')
+    .pipe(htmlmin({
+      removeComments: true,
+      collapseWhitespace: true
+    }))
+    .pipe(dest('dist'));
+}
+
+exports.html = html;
 
 function styles() {
   return src('src/styles/index.css')
